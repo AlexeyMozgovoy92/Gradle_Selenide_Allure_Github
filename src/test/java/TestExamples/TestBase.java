f7b245cb92d13ef67e13382b98637fc36908760c
Mozgovoy <alex.mzg@gmail.com>
2022-11-29 15:02:47 -0500

    added selenoid

--- src/test/java/TestExamples/TestBase.java
+++ src/test/java/TestExamples/TestBase.java
@@ -1,2 +1,24 @@
-package TestExamples;public class TestBase {
+package TestExamples;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class TestBase {
+    @BeforeAll
+    static void setUp() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+
+        Configuration.browserCapabilities = capabilities;
+        Configuration.baseUrl = "https://github.com";
+        Configuration.browserSize = "1920x1080";
+        Configuration.remote = "http://localhost:8090/wd/hub";
+    }
+
 }
